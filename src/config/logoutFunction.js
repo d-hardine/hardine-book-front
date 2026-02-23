3b1f361e3e3150678d1d842a2f62f07b59372402
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2026-02-23 09:31:59 +0700

    edited the readme file and made the logout faster

--- src/config/logoutFunction.js
+++ src/config/logoutFunction.js
@@ -2,13 +2,14 @@ import axiosInstance from "./axiosInstance"
 import socket from './socket.js'
 
 const logoutFunction = async (setUser, navigate) => {
+    setUser(null)
+    socket.disconnect()
+    localStorage.removeItem('isLoggedIn')
+    navigate('/', { replace: true })
     try {
-        const logoutResponse = await axiosInstance.post('/logout')
+        const logoutResponse = axiosInstance.post('/logout') // request in background without "await" 
         if(logoutResponse.status === 200) {
-            setUser(null)
-            socket.disconnect()
-            localStorage.removeItem('isLoggedIn')
-            navigate('/')
+            console.log('logged out successfully')
         }
     } catch (err) {
         console.error('Logout failed: ', err)
